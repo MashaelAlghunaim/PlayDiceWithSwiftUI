8a7e8adfae5d2b681f9e122fd11d835365fe7990
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2022-10-29 16:24:26 +0300

    Initial Commit

---
+++ PlayDiceWithSwiftUI/PlayDiceWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayDiceWithSwiftUIApp.swift
+//  PlayDiceWithSwiftUI
+//
+//  Created by Mashael Alghunaim on 04/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlayDiceWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
