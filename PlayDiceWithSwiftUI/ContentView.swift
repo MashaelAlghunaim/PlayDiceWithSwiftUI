251425d114bcc7f8e281967abc6cf639e6857712
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2022-10-30 09:21:38 +0300

    PlayWithDice

--- PlayDiceWithSwiftUI/ContentView.swift
+++ PlayDiceWithSwiftUI/ContentView.swift
@@ -8,19 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            ZStack{
+                Image("background").resizable().edgesIgnoringSafeArea(.all)
+                VStack {
+                    Image("diceeLogo")
+                    Spacer()
+                    HStack {
+                        DiceView(n: leftDiceNumber)
+                        DiceView(n: rightDiceNumber)
+                    }
+                    .padding(.horizontal)
+                    Spacer()
+                    Button("Roll") {
+                        leftDiceNumber = Int.random(in: 1...6)
+                        rightDiceNumber = Int.random(in: 1...6)
+                        
+                    }
+                    
+                    .font(.system(size: 50))
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                    .background(Color.red)
+                    .padding()
+                   
+                
+                }
+            
+            }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)").resizable().aspectRatio(1, contentMode: .fit)
+            .padding()
     }
 }
